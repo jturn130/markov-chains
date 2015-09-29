b75a72d7950a301043f7c27c3fd81b6d97787eec
Hackbright Student <info@hackbrightacademy.com>
2015-09-29 16:04:46 -0700

    working on ngram updated make chains

--- markov.py
+++ markov.py
@@ -1,4 +1,11 @@
+"""At command line provide <file_path> and <n-gram>
+ex: python markov.py corpus.txt 3 --> produces 3-word Markov chains
+"""
 from random import choice
+import sys
+
+input_path = sys.argv[1]
+n_gram = int(sys.argv[2])
 
 
 def open_and_read_file(file_path):
@@ -23,16 +30,16 @@ def open_and_read_file(file_path):
     # print all_text       
     # return all_text
 
-def make_chains(text_string):
-    """Takes input text as string; returns dictionary of markov chains.
+def make_chains(text_string, n):
+    """Takes input text as string; returns dictionary of n-length markov chains.
 
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
-    words in the input text.
+    A chain will be a key that consists of a n-tuple of (word1, word2, ..., wordn)
+    and the value would be a list of the word(s) that follow those n words in the
+    input text.
 
     For example:
 
-        >>> make_chains("hi there mary hi there juanita")
+        >>> make_chains("hi there mary hi there juanita", 2)
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
 
@@ -41,12 +48,26 @@ def make_chains(text_string):
     # your code goes here
     words = text_string.split()
     # # print words
-    for i in range(len(words)-2):
-        word_key = (words[i], words[i+1])
-        if word_key in chains:
-            chains[word_key].append(words[i+2]) 
+
+    word_key = words[:n]
+    print word_key
+    tuple_key = tuple(word_key)
+    print "This is a tuple key: {}".format(tuple_key)
+
+    for i in range(len(words)-n): #update range, probably n?
+        # create empty list to hold n-length chain
+        # iterate and append word to key list
+        # to create next list: slice first item off list and append next_word
+        # word_key = (words[i], words[i+1])
+        if tuple_key in chains:
+            chains[tuple_key].append(words[i+n]) 
         else:
-            chains[word_key] = [words[i+2]]
+            chains[tuple_key] = [words[i+n]]
+        # take all of word_key except the first item
+        word_key = word_key[1:]
+        word_key.append(words[i+n])
+        print "This is the updated word_key: {}".format(word_key)
+        tuple_key = tuple(word_key)
 
     return chains
 
@@ -81,13 +102,14 @@ def make_text(chains):
 
 # input_path = "green-eggs.txt"
 # input_path = "gettysburg.txt"
-input_path = "kanye.txt"
+# input_path = "kanye_lincoln.txt"
+# input_path = "esb.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, n_gram)
 
 # Produce random text
 random_text = make_text(chains)
